0b7d9fb2bfa2466d44030907a106e80886141a4b
Kroese <kroese@users.noreply.github.com>
2024-04-24 12:49:00 +0200

    Update request.rs

--- crates/api_common/src/request.rs
+++ crates/api_common/src/request.rs
@@ -111,14 +111,12 @@ pub fn generate_post_link_metadata(
       federated_thumbnail
     }
     // Generate local thumbnail if allowed
-    else if allow_generate_thumbnail && !is_image_post {
-      match metadata.opengraph_data.image {
-        Some(url) => generate_pictrs_thumbnail(&url, &context).await.ok(),
-        None => None,
-      }
-    }
-    else if allow_generate_thumbnail && is_image_post {
-      match post.url {
+    else if allow_generate_thumbnail {
+      match post
+        .url
+        .filter(|_| is_image_post)
+        .or(metadata.opengraph_data.image)
+      {
         Some(url) => generate_pictrs_thumbnail(&url, &context).await.ok(),
         None => None,
       }
