e4bf93bb11b17145818698ae598deeedc179e5f6
Kroese <kroese@users.noreply.github.com>
2024-05-27 20:39:04 +0200

    Update post.rs

--- crates/apub/src/objects/post.rs
+++ crates/apub/src/objects/post.rs
@@ -243,7 +243,7 @@ impl Object for ApubPost {
       .updated(page.updated.map(Into::into))
       .deleted(Some(false))
       .nsfw(page.sensitive)
-      .thumbnail_url(thumbnail_url.clone().into())
+      .thumbnail_url(thumbnail_url.map(Into::into))
       .ap_id(Some(page.id.clone().into()))
       .local(Some(false))
       .language_id(language_id)
